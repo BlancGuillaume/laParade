bbc56b18f925058ba05a367b06f164c20a30f284
elise givelet <elise givelet>
2016-01-26 16:54:17 +0100

    restructuration de la classe accessBD : ajout fonctions set_requete, get_requete et deconnect

--- bd/accessBD.php
+++ bd/accessBD.php
@@ -11,27 +11,50 @@ class accessBD {
     // Ajout de $num articles de type $artnr au panier
 
     function connect() {
-        $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
-        // Check connection
-        if ($this->conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        } 
+        $this->conn = mysql_connect($this->servername,$this->username,$this->password) or die ("erreur de connexion");
+        mysql_select_db($this->dbname,$this->conn) or die ("erreur de connexion base");
+        return $this->conn;
+    }
+
+    function set_requete($requete) { // pour INSERT, UPDATE...
+        // Exécution de la requête
+        // echo $requete . '<br />';
+        $result = mysql_query($requete);
+
+        return $result;
+    }
+
+    function get_requete($requete) { // pour SELECT
+        // Exécution de la requête
+        $result = mysql_query($requete);
+        $array = array();
+
+        while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+            array_push($array, $row);
+        }
+
+        mysql_free_result($result);
+        return $array;
+    }
+
+    function deconnect() {
+        mysql_close($this->conn);
     }
 
     // Suppression de $num articles du type $artnr du panier
 
-    function insert_utilisateur($id, $mdp, $isAdmin) {
-        // CREATION TABLE UTILISATEUR
-        $sql = "INSERT INTO UTILISATEUR (idUtilisateur, mdpUtilisateur, isAdmin) VALUES ('$id', '$mdp', '$isAdmin')";
+    // function insert_utilisateur($id, $mdp, $isAdmin) {
+    //     // CREATION TABLE UTILISATEUR
+    //     $sql = "INSERT INTO UTILISATEUR (idUtilisateur, mdpUtilisateur, isAdmin) VALUES ('$id', '$mdp', '$isAdmin')";
 
-        if ($this->conn->query($sql) === TRUE) {
-            echo "Ajout utilisateur effectuée";
-            return 0;
-        } else {
-            echo "Erreur : " . $this->conn->error;
-            return -1;
-        }
-    }
+    //     if ($this->conn->query($sql) === TRUE) {
+    //         echo "Ajout utilisateur effectuée";
+    //         return 0;
+    //     } else {
+    //         echo "Erreur : " . $this->conn->error;
+    //         return -1;
+    //     }
+    // }
 	
 	/*
 	function insert_reservation_client($mailClient, $nomClient, $prenomClient, $numClient, $dateLimiteReservation, $commentaireReservation,     ) {
@@ -64,15 +87,30 @@ class accessBD {
 	
 	
 
-    function get_utilisateur($id) {
-        $sql = "SELECT * FROM UTILISATEUR WHERE idUtilisateur='$id'";
+    // function get_utilisateur($id) {
+    //     $sql = "SELECT * FROM UTILISATEUR WHERE idUtilisateur='$id'";
 
-        if ($result = $this->conn->query($sql)) {
-            return $result->fetch_assoc();
-        } else {
-            echo "Erreur : " . $this->conn->error;
-            return -1;
-        }
-    }
+    //     if ($result = $this->conn->query($sql)) {
+    //         return $result->fetch_assoc();
+    //     } else {
+    //         echo "Erreur : " . $this->conn->error;
+    //         return -1;
+    //     }
+    // }
+
+    // function get_news() {
+    //     $i = 0;
+    //     $result = array();
+    //     $sql = mysql_query('SELECT idNews, nomNews, contenuNews, imageNews, lienNews FROM NEWS');
+    //     while (mysql_fetch_array($sql) = $row) {
+    //         $result[$i]['idNews'] = $row['idNews'];
+    //         $result[$i]['nomNews'] = $row['nomNews'];
+    //         $result[$i]['contenuNews'] = $row['contenuNews'];
+    //         $result[$i]['imageNews'] = $row['imageNews'];
+    //         $result[$i]['lienNews'] = $row['lienNews'];
+    //         $i++;
+    //     }
+    //     return $result;
+    // }
 }
 ?>
